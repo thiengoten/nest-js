be812f92ef54b108c126219543eaf91eb501ec2a
Phung Anh Khoa <thiengoten6@gmail.com>
2024-09-15 02:14:02 +0700

    feat: 🎸 adding supabase datbase url for host database

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,21 +3,21 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  const user = await prisma.user.create({
-    data: {
-      email: 'alex@ruheni.com',
-      password: 'password',
-    },
-  });
-  console.log(user);
-  const budget = await prisma.budget.create({
-    data: {
-      name: 'My Budget 22',
-      amount: 2000,
-      userId: user.id,
-    },
-  });
-  console.log(budget);
+  // const user = await prisma.user.create({
+  //   data: {
+  //     email: 'alex@ruheni.com',
+  //     password: 'password',
+  //   },
+  // });
+  // console.log(user);
+  // const budget = await prisma.budget.create({
+  //   data: {
+  //     name: 'My Budget 22',
+  //     amount: 2000,
+  //     userId: user.id,
+  //   },
+  // });
+  // console.log(budget);
 }
 
 main()
